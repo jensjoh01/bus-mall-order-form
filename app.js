a17a4a36f68bbd05ce2d7305fc608e2552e8c7ab
jensjoh01 <johnnyj2439@gmail.com>
2017-07-27 18:05:36 -0700

    working product?

--- app.js
+++ app.js
@@ -1,39 +1,69 @@
 'use strict';
 
-var allImages = [];
 
-
-function Image(name, altName){
-  this.productName = productName;
-  this.altNames = altName;
-  this.source = 'img' + this.name;
-  this.timeshown = 0;
+function Image(name, altName) {
+  this.name = name;
+  this.altName = altName;
+  this.source = 'img/' + this.name;
+  this.timesShown = 0;
   this.timesClicked = 0;
   this.timesOrdered = 0;
   Image.all.push(this);
 }
+// Store my objects, need to call by parameter name
+Image.all = [];
+//Store my initial file names
+Image.allNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 
-Image.altName = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg','img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
+Image.altNames = ['R2D2 Bags', 'Banana Slicer', 'Ipad/Toilet Paper Holder', 'Sweet Rain Boots', 'All-in-one breakfast', 'Meatball Bubblegum', 'Red Chair', 'Cthulhu', 'Duck Face Dogie', 'Dragon Meat', 'Pen-Utensil!', 'Pet Sweepers', 'Pizza Scissors', 'Shark Sleeping Bag', 'Sweeping Baby', 'Tauntaun', 'Unicorn Meat', 'USB creature', 'Water Can', 'Wine Glass'];
 
-Image.allName = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'breakfast', 'chair', 'cthulhu', 'dogDuck', 'dragon', 'pen', 'petSweep', 'scissors', 'shark', 'sweep' ,'tauntaun' ,'unicorn' ,'usb' ,'waterCan' ,'wineGlass'];
-
-
-for (var i = 0; i < allImages.length; i++){
-  new Image(image.allNames[0], image.altName[i]);
-};
-
-var orderEl = document.getElementById('order-list');
-var liEl = document.createElement('li');
-liEl.textContent = allImages[i].altName;
-orderEl.appendchild(lieEl);
-
-
-function addToCart(event){
-  var amountToBuy === document.getElementById('cart').value;
-  var itemOrdered = document.getElementById('selectItem').value
-  for(var i = 0; i < image.all.length; i++){
-    if(Image.all[i].altName === itemOrdered){
-
-    }
-  }
+if(localStorage.getItem('Image.all')){
+  console.log('I found something!');
+  Image.all = JSON.parse(localStorage.getItem('Image.all'));
+} else {
+  console.log('No data found, creating new instances');
+  for (var i = 0; i < Image.allNames.length; i++) {
+    new Image(Image.allNames[i], Image.altNames[i]);
+  };
 }
+
+//
+// /*====================== Get Item, add Quant to cart ============*/
+//
+// function addToCart(event) {
+//   var amountToBuy = document.getElementById('cart').value;
+//   var itemOrdered = document.getElementById('selectItem').value;
+//   console.log('Item: ' + itemOrdered);
+//   console.log(amountToBuy);
+//   for(var i = 0; i < Image.all.length; i++){
+//     if(Image.all[i].altName === itemOrdered){
+//       console.log('found the item, adding to cart');
+//       Image.all[i].timesOrdered += parseInt(amountToBuy);
+//       localStorage.setItem('Image.all',JSON.stringify(Image.all));
+//     }
+//   }
+// }
+//
+// /* =============== Get info from local storage append to list ==*/
+//
+// function showCart() {
+//   Image.all = JSON.parse(localStorage.getItem('Image.all'));
+//   console.log(Image.all[0].timesOrdered);
+//   var cartList = document.getElementById('order-list');
+//   var liEl = document.createElement('li');
+//   liEl.textContent = 'Your order for ' + Image.all[0].altName + ' Quantity: ' + Image.all[0].timesOrdered;
+//   cartList.appendChild(liEl);
+// }
+//
+// if(localStorage.getItem('Image.all')){
+//   console.log('I found something!');
+//   Image.all = JSON.parse(localStorage.getItem('Image.all'));
+// } else {
+//   console.log('No data found, creating new instances');
+//   for (var i = 0; i < Image.allNames.length; i++) {
+//       new Image(Image.allNames[i], Image.altNames[i]);
+//   };
+// }
+//
+// document.getElementById('add-to-cart').addEventListener('click',addToCart);
+// document.getElementById('showCart').addEventListener('click', showCart);
